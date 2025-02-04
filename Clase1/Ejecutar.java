34fae4ba994c2415ecd826bd8d9445b2a092a983
david178fav <109388229+david178fav@users.noreply.github.com>
2025-02-04 16:10:04 +0000

    se agrego uniajc

--- Clase1/Ejecutar.java
+++ Clase1/Ejecutar.java
@@ -1,7 +1,7 @@
 public class Ejecutar {
 
     public static void main(String[] args) {
-        System.out.println("hola mundo!");
+        System.out.println("hola mundo UNIAJC!");
     
     }
 }   
